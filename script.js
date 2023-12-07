29eabd4c07c335c4f7ef970b2b51c0e400273c2b
Dr.Dados <adnane.bachchar@gmail.com>
2023-12-07 08:24:19 +0100

    adding map and marker

--- script.js
+++ script.js
@@ -10,3 +10,52 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map = L.map('map').setView(coords, 13);
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
+    );
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
+    map.on('click', function (mapEvent) {
+      form.classList.remove('hidden');
+      inputDistance.focus();
+
+      console.log(lat, lng);
+    });
+  },
+  function () {
+    alert('Could not get the geolocation');
+  }
+);
+
+form.addEventListener('submit', function () {
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
